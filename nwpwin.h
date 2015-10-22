bbc36988440b312d3bf650036966b5423cc20891
skulenov <skulenov@gmail.com>
2015-10-22 13:57:29 +0200

    Dodao #define pozicija i velicina za glavni prozor

--- nwpwin.h
+++ nwpwin.h
@@ -1,6 +1,12 @@
 #include <windows.h>
 #include <string>
 
+//Moji define-ovi za ovu konkretnu aplikaciju:
+#define CW_XORIGIN 100
+#define CW_YORIGIN 100
+#define CW_WIDTH 300
+#define CW_HEIGHT 300
+
 class Application 
 {
 public:
@@ -20,7 +26,7 @@ public:
 	Window();
 
 	bool Create(HWND parent, DWORD style, PCSTR caption=0, int IdOrMenu=0, 
-		int x=CW_USEDEFAULT, int y=CW_USEDEFAULT, int width=CW_USEDEFAULT, int height=CW_USEDEFAULT);
+		int x=CW_XORIGIN, int y=CW_YORIGIN, int width=CW_WIDTH, int height=CW_HEIGHT);
 
 	operator HWND();
 	static LRESULT CALLBACK Proc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
