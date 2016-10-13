f566515fa409db1b6348c4706c95c5d898409af5
KapustaB <kapusta.borna.s@gmail.com>
2016-10-13 20:54:18 +0200

    Verzija - sve odjednom

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include "nwpwin.h"
 #include "res.h"
 
-// TODO: prepare classes (Edit, Button, ListBox) for child windows
-// TODO: derive from Window, override ClassName
+class Button :public Window {
+	public: std::string ClassName() {return "BUTTON";}
+};
+class Edit : public Window {
+	public: std::string ClassName() {return "EDIT";}
+};
+class ListBox : public Window {
+	public: std::string ClassName() {return "LISTBOX";}
+};
 
 class MainWindow : public Window
 {
@@ -14,28 +21,43 @@ protected:
 
 int MainWindow::OnCreate(CREATESTRUCT* pcs)
 {
-	// TODO: create all child windows
-	// TODO: disable "Remove" button
+	Button btn_add, btn_remove;
+	Edit label_edit;
+	ListBox list_box;
+
+	list_box.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "", IDC_LB, 10, 10, 180, 140);
+	label_edit.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "", IDC_EDIT, 200, 10, 100, 30);
+	btn_add.Create(*this, WS_CHILD | WS_VISIBLE, "Add", IDC_ADD, 200, 45, 100, 30);
+	btn_remove.Create(*this, WS_CHILD | WS_VISIBLE | WS_DISABLED, "Remove", IDC_REMOVE, 200, 80, 100, 30);
 	return 0;
 }
 
 void MainWindow::OnCommand(int id){
 	switch(id){
 		case ID_FILE_EXIT:
-			// TODO: close main window
+			DestroyWindow(*this);
 			break;
+
 		case ID_HELP_ABOUT:
-			// TODO: show dialog with text
+			MessageBox(NULL, "Meni treba vise pomoc nego tebi...", "HELP", MB_OK | MB_ICONINFORMATION);
 			break;
+
 		case IDC_ADD:
-			// TODO: get text from edit control
-			// TODO: add string to listbox control
-			// TODO: enable "Remove" button
+			char edit_text[70];
+			GetDlgItemText(*this, IDC_EDIT, edit_text, 70);	
+			SendDlgItemMessage(*this, IDC_LB, LB_ADDSTRING, NULL, (LPARAM)edit_text);
+			EnableWindow(GetDlgItem(*this, IDC_REMOVE), true);
 			break;
+
 		case IDC_REMOVE:
-			// TODO: get listbox selection
-			// TODO: if there is a selection, delete selected string
-			// TODO: disable "Remove" button if listbox is empty
+			int selected_item = SendDlgItemMessage(*this, IDC_LB, LB_GETCURSEL, NULL, NULL);
+			if (selected_item == LB_ERR) 
+				MessageBox(*this, "Select item from list", "Info", MB_ICONINFORMATION);
+			else
+				SendDlgItemMessage(*this, IDC_LB, LB_DELETESTRING, selected_item, NULL);
+		
+			if (!SendDlgItemMessage(*this, IDC_LB, LB_GETCOUNT, NULL, NULL))
+				EnableWindow(GetDlgItem(*this, IDC_REMOVE), false);
 			break;
 	}
 }
@@ -48,7 +70,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	HMENU hMenu = LoadMenu(hInstance, MAKEINTRESOURCE(IDM_V2));
 	MainWindow wnd; 
-	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 2", (int)hMenu);	
+	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 2", (int)hMenu, CW_USEDEFAULT, CW_USEDEFAULT, 350, 220);
 	// set icons
 	HICON hib = (HICON)LoadImage(hInstance, MAKEINTRESOURCE(IDI_V2), IMAGE_ICON, 32, 32, LR_DEFAULTCOLOR);
 	PostMessage(wnd, WM_SETICON, ICON_BIG, reinterpret_cast<LPARAM>(hib));
