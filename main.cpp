e4878eb66dfac211ac8777c717408c050ac6dc97
Perica Jurić <pejuric@vsite.hr>
2017-10-24 16:15:49 +0200

    Gotovo 2.0

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include "nwpwin.h"
 #include "res.h"
 
@@ -35,29 +36,36 @@ int MainWindow::OnCreate(CREATESTRUCT* pcs)
 	b1.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "Remove", IDC_REMOVE, 300, 140, 100, 30);
 	b2.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "Add", IDC_ADD, 300, 180, 100, 30);
 	lb.Create(*this, WS_CHILD | WS_VISIBLE |WS_BORDER, "",IDC_LB,100,100,100,100);
-	EnableWindow(GetDlgItem(*this,IDC_REMOVE),false);
+	EnableWindow(b1,false);
 	return 0;
 }
 ////////////////////////////////////////////////////////
 void MainWindow::OnCommand(int id){
 	switch(id){
 		case ID_FILE_EXIT:
-			CloseWindow(*this);
+			DestroyWindow(*this);
 			break;
 		case ID_HELP_ABOUT:
-			MessageBox(NULL,"Dialog in the box","Click ok",MB_OK|MB_ICONINFORMATION);
+			MessageBox(*this,"Dialog in the box","Click ok",MB_OK|MB_ICONINFORMATION);
 			break;
 		case IDC_ADD:
 			char msg[40];
 			GetDlgItemText(*this,IDC_EDIT,msg,40);
-			SendDlgItemMessage(*this,IDC_LB,LB_ADDSTRING,NULL,(LPARAM)msg);
-			EnableWindow(GetDlgItem(*this,IDC_REMOVE),true);
+			if (strlen(msg) != 0) {
+				SendDlgItemMessage(*this, IDC_LB, LB_ADDSTRING, NULL, (LPARAM)msg);
+				EnableWindow(GetDlgItem(*this, IDC_REMOVE), true);
+			}
 			break;
 		case IDC_REMOVE:
-			int abc = SendDlgItemMessage(*this, IDC_LB, LB_GETCURSEL, NULL, NULL);
-			SendDlgItemMessage(*this, IDC_LB, LB_DELETESTRING, (WPARAM)abc, NULL);
-			if (!SendDlgItemMessage(*this, IDC_LB, LB_GETCOUNT, NULL, NULL))
-				EnableWindow(GetDlgItem(*this,IDC_REMOVE),false);
+			int index = SendDlgItemMessage(*this, IDC_LB, LB_GETCURSEL, NULL, NULL);
+			if (index == -1) {
+				MessageBox(*this, "Select object to remove", "Alert", MB_OK | MB_ICONINFORMATION);
+			}
+			else{
+				SendDlgItemMessage(*this, IDC_LB, LB_DELETESTRING, (WPARAM)index, NULL);
+				if (!SendDlgItemMessage(*this, IDC_LB, LB_GETCOUNT, NULL, NULL))
+					EnableWindow(GetDlgItem(*this, IDC_REMOVE), false);
+			}
 			break;
 	}
 }
