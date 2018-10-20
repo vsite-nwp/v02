77aed51dd901a85e8431fccefe6a6a5633ce10bd
Mislav Piskac <mislav.piskac@vsite.hr>
2018-10-21 01:29:51 +0200

    Lab 2

--- main.cpp
+++ main.cpp
@@ -1,41 +1,119 @@
 #include "nwpwin.h"
 #include "res.h"
+#include <string>
 
-// TODO: prepare classes (Edit, Button, ListBox) for child windows
-// TODO: derive from Window, override ClassName
+class ListBox : public Window {
+protected:
+	std::string ClassName() override {
+		return "LISTBOX";
+	}
+};
 
-class MainWindow : public Window
-{
+class Edit : public Window {
+protected:
+	std::string ClassName() override {
+		return "EDIT";
+	}
+};
+
+class Button : public Window {
+protected:
+	std::string ClassName() override {
+		return "BUTTON";
+	}
+};
+
+class MainWindow : public Window {
 protected:
 	int OnCreate(CREATESTRUCT* pcs);
 	void OnCommand(int id);
 	void OnDestroy();
 };
 
+
+
+
 int MainWindow::OnCreate(CREATESTRUCT* pcs)
 {
-	// TODO: create all child windows
-	// TODO: disable "Remove" button
+	HFONT hf = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
+
+	ListBox listBox;
+	Edit edit;
+	Button buttonAdd, buttonRemove;
+
+	listBox.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "", IDC_LB, 50, 50, 100, 120);
+	edit.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "", IDC_EDIT, 170, 60, 80, 20);
+	buttonAdd.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "Add", IDC_ADD, 170, 100, 80, 20);
+	buttonRemove.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER | WS_DISABLED, "Remove", IDC_REMOVE, 170, 130, 80, 20);
+
+	SendMessage(edit, WM_SETFONT, (WPARAM)hf, 0);
+	
 	return 0;
 }
 
 void MainWindow::OnCommand(int id){
 	switch(id){
 		case ID_FILE_EXIT:
-			// TODO: close main window
+			{
+				int count = SendDlgItemMessage(*this, IDC_LB, LB_GETCOUNT, NULL, NULL);
+				
+				if (count > 0) {
+					std::string allItems = "Items: \"";
+					for (int index = 0; index < count; ++index) {
+						char text[101];
+						SendDlgItemMessage(*this, IDC_LB, LB_GETTEXT, index, (LPARAM)text);
+						if (index == count - 1) {
+							allItems += text + std::string("");
+						} else {
+						allItems += text + std::string(", ");
+						}
+					}
+					allItems += "\" will be lost!\nDo you want to exit?";
+
+					int result = MessageBox(*this, allItems.c_str(), "Close", MB_YESNO | MB_ICONQUESTION);
+					if (result == IDYES) {
+						::PostQuitMessage(0);
+					}
+				}
+				else {
+					::PostQuitMessage(0);
+				}
+			}
 			break;
 		case ID_HELP_ABOUT:
-			// TODO: show dialog with text
+			MessageBox(*this, "Exercise No. 2!", "About", MB_OK | MB_ICONINFORMATION);
 			break;
 		case IDC_ADD:
-			// TODO: get text from edit control
-			// TODO: add string to listbox control
-			// TODO: enable "Remove" button
+			char nameOfItem[101];
+			GetDlgItemText(*this, IDC_EDIT, nameOfItem, sizeof(nameOfItem));
+			if (strlen(nameOfItem)) {
+				//why this dont work?
+				//SendMessage(GetDlgItem(*this, IDC_REMOVE), WM_ENABLE, NULL, NULL);
+				EnableWindow(GetDlgItem(*this, IDC_REMOVE), true);
+				SendDlgItemMessage(*this, IDC_LB, LB_ADDSTRING, NULL, (LPARAM)nameOfItem);
+				SetWindowText(GetDlgItem(*this, IDC_EDIT), "");
+			} else {
+				MessageBox(*this, "Textfield is empty!", "Error", MB_OK | MB_ICONWARNING);
+			}
 			break;
 		case IDC_REMOVE:
-			// TODO: get listbox selection
-			// TODO: if there is a selection, delete selected string
-			// TODO: disable "Remove" button if listbox is empty
+			int selectedItem = SendDlgItemMessage(*this, IDC_LB, LB_GETCURSEL, NULL, NULL);
+			if (selectedItem != LB_ERR) {
+				char textOfSelectedItem[101];
+				SendDlgItemMessage(*this, IDC_LB, LB_GETTEXT, selectedItem, (LPARAM)textOfSelectedItem);
+
+				std::string messageDelete = std::string("Delete ") + textOfSelectedItem + "?";
+				int result = MessageBox(*this, messageDelete.c_str() , "Delete", MB_YESNO | MB_ICONQUESTION);
+				
+				if (result == IDYES) {
+					SendDlgItemMessage(*this, IDC_LB, LB_DELETESTRING, selectedItem, NULL);
+					if (!SendDlgItemMessage(*this, IDC_LB, LB_GETCOUNT, NULL, NULL)) {
+						EnableWindow(GetDlgItem(*this, IDC_REMOVE), false);
+					}
+				}				
+			} else {
+				MessageBox(*this, "No selected item!", "Error", MB_OK | MB_ICONINFORMATION);
+			}
 			break;
 	}
 }
@@ -49,7 +127,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 	HMENU hMenu = LoadMenu(hInstance, MAKEINTRESOURCE(IDM_V2));
 	MainWindow wnd; 
 	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 2", (int)hMenu);	
-	// set icons
+	
 	HICON hib = (HICON)LoadImage(hInstance, MAKEINTRESOURCE(IDI_V2), IMAGE_ICON, 32, 32, LR_DEFAULTCOLOR);
 	PostMessage(wnd, WM_SETICON, ICON_BIG, reinterpret_cast<LPARAM>(hib));
 	HICON his = (HICON)LoadImage(hInstance, MAKEINTRESOURCE(IDI_V2), IMAGE_ICON, 16, 16, LR_DEFAULTCOLOR);
