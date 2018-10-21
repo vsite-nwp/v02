4489bc0b9979de6dcbe89cff2f43a9a335935f60
Mislav Piskac <mislav.piskac@vsite.hr>
2018-10-21 17:06:01 +0200

    Podijelio kod u dodatne funkcije

--- main.cpp
+++ main.cpp
@@ -24,14 +24,18 @@ protected:
 };
 
 class MainWindow : public Window {
+private:
+	std::string ItemsToString(const int count);
+	void OnExit();
+	void OnAdd();
+	void OnRemove(const int selectedItem);
+	void RemoveItem(const int selectedItem);
+	void AddItem(const char* nameOfItem);
 protected:
 	int OnCreate(CREATESTRUCT* pcs);
 	void OnCommand(int id);
 	void OnDestroy();
 };
-
-
-
 
 int MainWindow::OnCreate(CREATESTRUCT* pcs)
 {
@@ -51,66 +55,88 @@ int MainWindow::OnCreate(CREATESTRUCT* pcs)
 	return 0;
 }
 
+void MainWindow::OnExit() {
+	int count = SendDlgItemMessage(*this, IDC_LB, LB_GETCOUNT, NULL, NULL);
+
+	if (count > 0) {
+		int result = MessageBox(*this, ItemsToString(count).c_str(), "Close", MB_YESNO | MB_ICONQUESTION);
+		if (result == IDYES) {
+			::PostQuitMessage(0);
+		}
+	}
+	else {
+		::PostQuitMessage(0);
+	}
+}
+
+std::string MainWindow::ItemsToString(const int count) {
+	std::string allItems = "Items: \"";
+	for (int index = 0; index < count; ++index) {
+		char text[101];
+		SendDlgItemMessage(*this, IDC_LB, LB_GETTEXT, index, (LPARAM)text);
+		if (index == count - 1) {
+			allItems += text + std::string("");
+		}
+		else {
+			allItems += text + std::string(", ");
+		}
+	}
+	allItems += "\" will be lost!\nDo you want to exit?";
+
+	return allItems;
+}
+
+void MainWindow::AddItem(const char* nameOfItem) {
+	EnableWindow(GetDlgItem(*this, IDC_REMOVE), true);
+	SendDlgItemMessage(*this, IDC_LB, LB_ADDSTRING, NULL, (LPARAM)nameOfItem);
+	SetWindowText(GetDlgItem(*this, IDC_EDIT), "");
+}
+
+void MainWindow::OnAdd() {
+	char nameOfItem[101];
+	GetDlgItemText(*this, IDC_EDIT, nameOfItem, sizeof(nameOfItem));
+	if (strlen(nameOfItem)) {
+		AddItem(nameOfItem);
+	}
+	else {
+		MessageBox(*this, "Textfield is empty!", "Error", MB_OK | MB_ICONWARNING);
+	}
+}
+
+void MainWindow::OnRemove(const int selectedItem) {
+	char textOfSelectedItem[101];
+	SendDlgItemMessage(*this, IDC_LB, LB_GETTEXT, selectedItem, (LPARAM)textOfSelectedItem);
+
+	std::string messageDelete = std::string("Delete ") + textOfSelectedItem + "?";
+	int result = MessageBox(*this, messageDelete.c_str(), "Delete", MB_YESNO | MB_ICONQUESTION);
+
+	if (result == IDYES) {
+		RemoveItem(selectedItem);
+	}	
+}
+
+void MainWindow::RemoveItem(const int selectedItem) {
+	SendDlgItemMessage(*this, IDC_LB, LB_DELETESTRING, selectedItem, NULL);
+	if (!SendDlgItemMessage(*this, IDC_LB, LB_GETCOUNT, NULL, NULL)) {
+		EnableWindow(GetDlgItem(*this, IDC_REMOVE), false);
+	}
+}
+
 void MainWindow::OnCommand(int id){
 	switch(id){
 		case ID_FILE_EXIT:
-			{
-				int count = SendDlgItemMessage(*this, IDC_LB, LB_GETCOUNT, NULL, NULL);
-				
-				if (count > 0) {
-					std::string allItems = "Items: \"";
-					for (int index = 0; index < count; ++index) {
-						char text[101];
-						SendDlgItemMessage(*this, IDC_LB, LB_GETTEXT, index, (LPARAM)text);
-						if (index == count - 1) {
-							allItems += text + std::string("");
-						} else {
-						allItems += text + std::string(", ");
-						}
-					}
-					allItems += "\" will be lost!\nDo you want to exit?";
-
-					int result = MessageBox(*this, allItems.c_str(), "Close", MB_YESNO | MB_ICONQUESTION);
-					if (result == IDYES) {
-						::PostQuitMessage(0);
-					}
-				}
-				else {
-					::PostQuitMessage(0);
-				}
-			}
+			OnExit();
 			break;
 		case ID_HELP_ABOUT:
 			MessageBox(*this, "Exercise No. 2!", "About", MB_OK | MB_ICONINFORMATION);
 			break;
 		case IDC_ADD:
-			char nameOfItem[101];
-			GetDlgItemText(*this, IDC_EDIT, nameOfItem, sizeof(nameOfItem));
-			if (strlen(nameOfItem)) {
-				//why this dont work?
-				//SendMessage(GetDlgItem(*this, IDC_REMOVE), WM_ENABLE, NULL, NULL);
-				EnableWindow(GetDlgItem(*this, IDC_REMOVE), true);
-				SendDlgItemMessage(*this, IDC_LB, LB_ADDSTRING, NULL, (LPARAM)nameOfItem);
-				SetWindowText(GetDlgItem(*this, IDC_EDIT), "");
-			} else {
-				MessageBox(*this, "Textfield is empty!", "Error", MB_OK | MB_ICONWARNING);
-			}
+			OnAdd();
 			break;
 		case IDC_REMOVE:
 			int selectedItem = SendDlgItemMessage(*this, IDC_LB, LB_GETCURSEL, NULL, NULL);
 			if (selectedItem != LB_ERR) {
-				char textOfSelectedItem[101];
-				SendDlgItemMessage(*this, IDC_LB, LB_GETTEXT, selectedItem, (LPARAM)textOfSelectedItem);
-
-				std::string messageDelete = std::string("Delete ") + textOfSelectedItem + "?";
-				int result = MessageBox(*this, messageDelete.c_str() , "Delete", MB_YESNO | MB_ICONQUESTION);
-				
-				if (result == IDYES) {
-					SendDlgItemMessage(*this, IDC_LB, LB_DELETESTRING, selectedItem, NULL);
-					if (!SendDlgItemMessage(*this, IDC_LB, LB_GETCOUNT, NULL, NULL)) {
-						EnableWindow(GetDlgItem(*this, IDC_REMOVE), false);
-					}
-				}				
+				OnRemove(selectedItem);
 			} else {
 				MessageBox(*this, "No selected item!", "Error", MB_OK | MB_ICONINFORMATION);
 			}
