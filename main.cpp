f9a08f89f5fb8dbb04c2126eb0d6a1ec47c099f4
Ivan Blažeka <ivblazek@vsite.hr>
2019-10-26 16:07:30 +0200

    Implement menu items

--- main.cpp
+++ main.cpp
@@ -49,10 +49,10 @@ int MainWindow::OnCreate(CREATESTRUCT* pcs)
 void MainWindow::OnCommand(int id){
 	switch(id){
 		case ID_FILE_EXIT:
-			// TODO: close main window
+			DestroyWindow(*this);
 			break;
 		case ID_HELP_ABOUT:
-			// TODO: show dialog with text
+			MessageBox(*this, "Poruka", "About", MB_OK);
 			break;
 		case IDC_ADD:
 			// TODO: get text from edit control
