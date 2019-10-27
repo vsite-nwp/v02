692522aeb1074b2ed6037278a343d9c81c46ad27
Josip Vučković <39704449+JosipVuckovic@users.noreply.github.com>
2019-10-27 21:07:29 +0100

    vj2 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include "nwpwin.h"
 #include "res.h"
 
-// TODO: prepare classes (Edit, Button, ListBox) for child windows
-// TODO: derive from Window, override ClassName
+class Button : public Window
+{
+private:
+	std::string ClassName()
+	{
+		return "Button";
+	}
+};
+
+class Edit : public Window
+{
+private:
+	std::string ClassName()
+	{
+		return "Edit";
+	}
+};
+
+class ListBox : public Window
+{
+private:
+	std::string ClassName()
+	{
+		return "ListBox";
+	}
+};
 
 class MainWindow : public Window
 {
@@ -10,51 +34,95 @@ protected:
 	int OnCreate(CREATESTRUCT* pcs);
 	void OnCommand(int id);
 	void OnDestroy();
+	void onAbout();
+	void onAdd();
+	void onRemove();
+	void onExit();
 };
 
 int MainWindow::OnCreate(CREATESTRUCT* pcs)
 {
-	// TODO: create all child windows
-	// TODO: disable "Remove" button
+	
+	Button mButtonAdd, mButtonRemove;
+	ListBox mListBox;
+	Edit mEdit;
+
+	mButtonAdd.Create(*this, WS_CHILD | WS_VISIBLE, "Add", IDC_ADD, 200, 70, 100, 20);
+	mButtonRemove.Create(*this, WS_CHILD | WS_VISIBLE | WS_DISABLED, "Remove", IDC_REMOVE, 200, 90, 100, 20);
+	mEdit.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "", IDC_EDIT, 200, 50, 100, 20);
+	mListBox.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "", IDC_LB, 50, 50, 150, 150);
+
 	return 0;
 }
 
 void MainWindow::OnCommand(int id){
 	switch(id){
 		case ID_FILE_EXIT:
-			// TODO: close main window
+			onExit();
 			break;
 		case ID_HELP_ABOUT:
-			// TODO: show dialog with text
+			onAbout();
 			break;
 		case IDC_ADD:
-			// TODO: get text from edit control
-			// TODO: add string to listbox control
-			// TODO: enable "Remove" button
+			onAdd();
 			break;
 		case IDC_REMOVE:
-			// TODO: get listbox selection
-			// TODO: if there is a selection, delete selected string
-			// TODO: disable "Remove" button if listbox is empty
+			onRemove();
 			break;
 	}
 }
 
-void MainWindow::OnDestroy(){
+void MainWindow::onAbout() 
+{
+	MessageBox((HWND)* this, "GUI program with buttons!! \n !Patent Pending!", "About", MB_OK | MB_ICONINFORMATION);
+}
+
+void MainWindow::onExit() 
+{
+	if (MessageBox(*this, "Do you want to exit?", "Exit?", MB_YESNOCANCEL | MB_ICONEXCLAMATION) == IDYES)
+	{
+		DestroyWindow(*this);
+	}
+}
+
+void MainWindow::onAdd() 
+{
+	char userInput[255+1];
+	if (GetDlgItemText(*this, IDC_EDIT, userInput, sizeof(userInput)))
+	{
+		SendDlgItemMessage(*this, IDC_LB, LB_ADDSTRING, 0, reinterpret_cast<LPARAM>(userInput));
+		EnableWindow(GetDlgItem(*this, IDC_REMOVE), true);
+		SetDlgItemText(*this, IDC_EDIT, "");
+	}	   
+}
+
+void MainWindow::onRemove()
+{
+	long res = SendDlgItemMessage(*this, IDC_LB, LB_GETCURSEL, 0, 0);
+	if (res != LB_ERR) {
+		SendDlgItemMessage(*this, IDC_LB, LB_DELETESTRING, (WPARAM)res, 0);
+		if (!SendDlgItemMessage(*this, IDC_LB, LB_GETCOUNT, 0, 0))
+			EnableWindow(GetDlgItem(*this, IDC_REMOVE), false);
+	}
+}
+
+void MainWindow::OnDestroy()
+{
 	::PostQuitMessage(0);
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	HMENU hMenu = LoadMenu(hInstance, MAKEINTRESOURCE(IDM_V2));
-	MainWindow wnd; 
-	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 2", (int)hMenu);	
+	MainWindow wnd;
+	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 2", (int)hMenu);
 	// set icons
 	HICON hib = (HICON)LoadImage(hInstance, MAKEINTRESOURCE(IDI_V2), IMAGE_ICON, 32, 32, LR_DEFAULTCOLOR);
 	PostMessage(wnd, WM_SETICON, ICON_BIG, reinterpret_cast<LPARAM>(hib));
 	HICON his = (HICON)LoadImage(hInstance, MAKEINTRESOURCE(IDI_V2), IMAGE_ICON, 16, 16, LR_DEFAULTCOLOR);
 	PostMessage(wnd, WM_SETICON, ICON_SMALL, reinterpret_cast<LPARAM>(his));
 
-	Application app; 
+	Application app;
 	return app.Run();
 }
+
