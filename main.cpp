c4690fb9910364caa3569209e56f43f2f02bd77c
Sven Gotal <svgotal@vsite.hr>
2019-10-27 14:14:16 +0100

    Implement Add, Edit and Remove

--- main.cpp
+++ main.cpp
@@ -31,41 +31,53 @@ protected:
 int MainWindow::OnCreate(CREATESTRUCT* pcs)
 {
 	
-	Button b, c;
-	//b.Create(pcs->hwndParent,pcs->dwExStyle, pcs->lpszName, pcs->style, pcs->cx, pcs->cy, pcs->x, pcs->y);
-	b.Create(*this, WS_CHILD | WS_VISIBLE, "Add", IDC_ADD, 150 , 90 , 120, 30);
-	c.Create(*this, WS_CHILD | WS_VISIBLE, "Remove", IDC_REMOVE, 150 , 130, 120, 30);
+	Button Button_Add, Button_Remove;
+	Button_Add.Create(*this, WS_CHILD | WS_VISIBLE, "Add", IDC_ADD, 150 , 90 , 120, 30);
+	Button_Remove.Create(*this, WS_CHILD | WS_VISIBLE, "Remove", IDC_REMOVE, 150 , 130, 120, 30);
 
 	Edit e;
 	e.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "", IDC_EDIT, 150, 30, 120, 30);
 
 	ListBox lb;
-	lb.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "", IDC_LB, 30, 30, 50, 50);
+	lb.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "", IDC_LB, 30, 30, 90, 150);
 
-	
-
-
-	// TODO: disable "Remove" button
 	return 0;
 }
 
 void MainWindow::OnCommand(int id){
 	switch(id){
 		case ID_FILE_EXIT:
-			// TODO: close main window
+			DestroyWindow(*this);
 			break;
 		case ID_HELP_ABOUT:
-			// TODO: show dialog with text
+			MessageBox(*this, "Some msg", "About", MB_OK);
 			break;
 		case IDC_ADD:
-			// TODO: get text from edit control
-			// TODO: add string to listbox control
-			// TODO: enable "Remove" button
+			
+			char TextBox_Text[30];
+			if (GetDlgItemText(*this, IDC_EDIT, TextBox_Text, sizeof(TextBox_Text)))
+			{
+				SendDlgItemMessage(*this, IDC_LB, LB_ADDSTRING, 0, (LPARAM)TextBox_Text);
+			}
+			SetDlgItemText(*this, IDC_EDIT, "");
 			break;
 		case IDC_REMOVE:
-			// TODO: get listbox selection
-			// TODO: if there is a selection, delete selected string
+			LRESULT selected = SendDlgItemMessage(*this, IDC_LB, LB_GETCURSEL, 0, 0);
+
+			// if(selected) -> cannot delete first entry
+			if (selected != LB_ERR)
+			{
+				SendDlgItemMessage(*this, IDC_LB, LB_DELETESTRING, selected, 0);
+			}
+
 			// TODO: disable "Remove" button if listbox is empty
+			//HWND ListBox = GetDlgItem(*this, IDC_LB);
+			//HWND Button_Remove = GetDlgItem(*this, IDC_REMOVE);
+			//if (SendDlgItemMessage(ListBox, IDC_LB, LB_ERR, 0, 0) != LB_ERR)
+			//{
+			//	SendDlgItemMessage(Button_Remove, IDC_REMOVE,)
+			//}
+
 			break;
 	}
 }
