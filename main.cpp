70a5596e85f2f989e796516f968267252f04bf3d
Sven Gotal <svgotal@vsite.hr>
2019-10-28 17:33:46 +0100

    Implement Enable/Disable remove button

--- main.cpp
+++ main.cpp
@@ -30,10 +30,10 @@ protected:
 
 int MainWindow::OnCreate(CREATESTRUCT* pcs)
 {
-	
+
 	Button Button_Add, Button_Remove;
 	Button_Add.Create(*this, WS_CHILD | WS_VISIBLE, "Add", IDC_ADD, 150 , 90 , 120, 30);
-	Button_Remove.Create(*this, WS_CHILD | WS_VISIBLE, "Remove", IDC_REMOVE, 150 , 130, 120, 30);
+	Button_Remove.Create(*this, WS_CHILD | WS_VISIBLE | WS_DISABLED, "Remove", IDC_REMOVE, 150 , 130, 120, 30);
 
 	Edit e;
 	e.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "", IDC_EDIT, 150, 30, 120, 30);
@@ -41,10 +41,15 @@ int MainWindow::OnCreate(CREATESTRUCT* pcs)
 	ListBox lb;
 	lb.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "", IDC_LB, 30, 30, 90, 150);
 
+
+
 	return 0;
 }
 
 void MainWindow::OnCommand(int id){
+
+	
+
 	switch(id){
 		case ID_FILE_EXIT:
 			DestroyWindow(*this);
@@ -58,25 +63,28 @@ void MainWindow::OnCommand(int id){
 			if (GetDlgItemText(*this, IDC_EDIT, TextBox_Text, sizeof(TextBox_Text)))
 			{
 				SendDlgItemMessage(*this, IDC_LB, LB_ADDSTRING, 0, (LPARAM)TextBox_Text);
+				if (SendDlgItemMessage(*this, IDC_LB, LB_GETCOUNT, 0, 0) > 0)
+				{
+					EnableWindow(GetDlgItem(*this, IDC_REMOVE), TRUE);
+
+				}
+
 			}
 			SetDlgItemText(*this, IDC_EDIT, "");
 			break;
 		case IDC_REMOVE:
 			LRESULT selected = SendDlgItemMessage(*this, IDC_LB, LB_GETCURSEL, 0, 0);
 
+
 			// if(selected) -> cannot delete first entry
 			if (selected != LB_ERR)
 			{
 				SendDlgItemMessage(*this, IDC_LB, LB_DELETESTRING, selected, 0);
+				if (SendDlgItemMessage(*this, IDC_LB, LB_GETCOUNT, 0, 0) == 0)
+				{
+					EnableWindow(GetDlgItem(*this, IDC_REMOVE), FALSE);
+				}
 			}
-
-			// TODO: disable "Remove" button if listbox is empty
-			//HWND ListBox = GetDlgItem(*this, IDC_LB);
-			//HWND Button_Remove = GetDlgItem(*this, IDC_REMOVE);
-			//if (SendDlgItemMessage(ListBox, IDC_LB, LB_ERR, 0, 0) != LB_ERR)
-			//{
-			//	SendDlgItemMessage(Button_Remove, IDC_REMOVE,)
-			//}
 
 			break;
 	}
