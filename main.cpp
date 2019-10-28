4006e685e94ca3cbe03e41f8fdf278f0a3b8246a
Marko Boroš <Marko.Boros@vsite.hr>
2019-10-28 15:05:53 +0100

    Fix b1 creating both buttons, fix excess use of GetDlgItem for fetching the button to disable it,swap const value of 100 for sizeof buffer, swap sending wm_close for closewindow and ondestroy

--- main.cpp
+++ main.cpp
@@ -30,9 +30,9 @@ int MainWindow::OnCreate(CREATESTRUCT* pcs)
 	Edit e1;
 	e1.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "Input", IDC_EDIT, 200, 50, 100, 40);
 	b1.Create(*this, WS_CHILD | WS_VISIBLE, "Add", IDC_ADD, 200, 100, 100, 40);
-	b1.Create(*this, WS_CHILD | WS_VISIBLE, "Remove", IDC_REMOVE, 200, 150, 100, 40);
+	b2.Create(*this, WS_CHILD | WS_VISIBLE, "Remove", IDC_REMOVE, 200, 150, 100, 40);
 	lb1.Create(*this, WS_CHILD | WS_VISIBLE | WS_CAPTION, "List", IDC_LB, 100, 50, 100, 200);
-	EnableWindow(GetDlgItem(*this, IDC_REMOVE), false);
+	EnableWindow(b2, false);
 	return 0;
 }
 
@@ -40,7 +40,8 @@ void MainWindow::OnCommand(int id) {
 	switch (id) {
 	case ID_FILE_EXIT:
 		if(MessageBox(*this, "Are you sure?", "Exit?", MB_YESNO| MB_ICONQUESTION)== IDYES) {
-			SendMessage(*this, WM_CLOSE, NULL, NULL);
+			CloseWindow(*this);
+			OnDestroy();
 		}
 		break;
 	case ID_HELP_ABOUT:
@@ -48,21 +49,21 @@ void MainWindow::OnCommand(int id) {
 		break;
 	case IDC_ADD:
 		char stringy[100];
-		if (GetDlgItemTextA(*this, IDC_EDIT, stringy, 100) != 0) {
-			SendDlgItemMessageA(*this, IDC_LB, LB_ADDSTRING, NULL, (LPARAM)stringy);
+		if (GetDlgItemTextA(*this, IDC_EDIT, stringy, sizeof(stringy)) != 0) {
+			SendDlgItemMessage(*this, IDC_LB, LB_ADDSTRING, NULL, (LPARAM)stringy);
 			EnableWindow(GetDlgItem(*this, IDC_REMOVE), true);
 		}
 		break;
 	case IDC_REMOVE:
-		LRESULT handleitem = SendDlgItemMessageA(*this, IDC_LB, LB_GETCURSEL, NULL, NULL);
+		LRESULT handleitem = SendDlgItemMessage(*this, IDC_LB, LB_GETCURSEL, NULL, NULL);
 		if (handleitem == LB_ERR) {
 			MessageBox(*this, "No item selected", "Silly goose", MB_RIGHT);
 			break;
 		}
 		else
 		{
-			SendDlgItemMessageA(*this, IDC_LB, LB_DELETESTRING, handleitem, NULL);
-			if (SendDlgItemMessageA(*this, IDC_LB, LB_GETCOUNT, NULL, NULL) == 0){
+			SendDlgItemMessage(*this, IDC_LB, LB_DELETESTRING, handleitem, NULL);
+			if (SendDlgItemMessage(*this, IDC_LB, LB_GETCOUNT, NULL, NULL) == 0){
 				EnableWindow(GetDlgItem(*this, IDC_REMOVE), false);
 			}
 		}
@@ -79,7 +80,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 	HMENU hMenu = LoadMenu(hInstance, MAKEINTRESOURCE(IDM_V2));
 	MainWindow wnd; 
 	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 2", (int)hMenu);	
-	// set icons
 	HICON hib = (HICON)LoadImage(hInstance, MAKEINTRESOURCE(IDI_V2), IMAGE_ICON, 32, 32, LR_DEFAULTCOLOR);
 	PostMessage(wnd, WM_SETICON, ICON_BIG, reinterpret_cast<LPARAM>(hib));
 	HICON his = (HICON)LoadImage(hInstance, MAKEINTRESOURCE(IDI_V2), IMAGE_ICON, 16, 16, LR_DEFAULTCOLOR);
