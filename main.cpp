0e38baa1eb7f78e16a8965250f0ae6d61faa31d0
Senad Elezović <sen.elezovic@gmail.com>
2019-10-31 15:31:25 +0100

    Ispričavam se na kašnjenju, nije mi laptop bio u funkciji. Točnije, izgubio sam punjač te za vikend nisam uspio riješiti zadatak kako sam planirao.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include "nwpwin.h"
 #include "res.h"
 
-// TODO: prepare classes (Edit, Button, ListBox) for child windows
-// TODO: derive from Window, override ClassName
 
+class Edit : public Window {
+public:
+	std::string ClassName() { return "EDIT";};
+};
+class Button : public Window {
+	std::string ClassName() { return "BUTTON"; };
+
+};
+class ListBox : public Window {
+	std::string ClassName() { return "LISTBOX"; };
+
+};
 class MainWindow : public Window
 {
 protected:
@@ -14,28 +24,40 @@ protected:
 
 int MainWindow::OnCreate(CREATESTRUCT* pcs)
 {
-	// TODO: create all child windows
-	// TODO: disable "Remove" button
+	ListBox lb;
+	Edit edit;
+	Button add, rem;
+	lb.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER,"listbox", IDC_LB, 0, 0, 100, 100);
+	add.Create(*this, WS_CHILD | WS_VISIBLE, "add", IDC_ADD, 100, 60,70, 20);
+	rem.Create(*this, WS_CHILD | WS_VISIBLE | WS_DISABLED, "remove", IDC_REMOVE, 100, 30, 70, 20);
+	edit.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "", IDC_EDIT, 100, 0, 70, 20);
 	return 0;
 }
 
 void MainWindow::OnCommand(int id){
+	LPSTR text = new TCHAR[15];
+	HWND lb = GetDlgItem(*this, IDC_LB);
+	HWND rem = GetDlgItem(*this, IDC_REMOVE);
 	switch(id){
 		case ID_FILE_EXIT:
-			// TODO: close main window
+			PostQuitMessage(0);
 			break;
 		case ID_HELP_ABOUT:
+			MessageBox(*this, (LPCSTR)"this is help", (LPCSTR)"HELP", MB_HELP);
 			// TODO: show dialog with text
 			break;
 		case IDC_ADD:
-			// TODO: get text from edit control
-			// TODO: add string to listbox control
-			// TODO: enable "Remove" button
+			
+			GetDlgItemText(*this, IDC_EDIT,(LPSTR) text, 15);
+			SendMessage(lb, LB_ADDSTRING, 0, (LPARAM) text);
+			EnableWindow(rem, TRUE);
 			break;
 		case IDC_REMOVE:
-			// TODO: get listbox selection
-			// TODO: if there is a selection, delete selected string
-			// TODO: disable "Remove" button if listbox is empty
+			int index = SendMessage(lb, LB_GETCURSEL, 0, 0);
+			SendMessage(lb, LB_DELETESTRING, (WPARAM)index, 0);
+			if (SendMessage(lb, LB_GETCOUNT, 0, 0) == 0) {
+				EnableWindow(rem, FALSE);
+			}
 			break;
 	}
 }
@@ -46,6 +68,7 @@ void MainWindow::OnDestroy(){
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
+
 	HMENU hMenu = LoadMenu(hInstance, MAKEINTRESOURCE(IDM_V2));
 	MainWindow wnd; 
 	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 2", (int)hMenu);	
