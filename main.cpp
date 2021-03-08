5bbbd82dc0c44e320fd95836bc8935ea6cae8f3e
Topesa <pesa710@gmail.com>
2021-03-08 23:21:51 +0100

    Resolve everything except "case IDC_REMOVE"

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include "nwpwin.h"
 #include "res.h"
 
-// TODO: prepare classes (Edit, Button, ListBox) for child windows
-// TODO: derive from Window, override ClassName
+class Edit :public Window {
+public:
+	std::string ClassName() override { return "EDIT"; }
+};
+
+class Button :public Window {
+public:
+	std::string ClassName() override { return "BUTTON"; }
+};
+
+class ListBox:public Window {
+public:
+	std::string ClassName() override { return "LISTBOX"; }
+};
 
 class MainWindow : public Window
 {
@@ -14,23 +26,31 @@ protected:
 
 int MainWindow::OnCreate(CREATESTRUCT* pcs)
 {
-	// TODO: create all child windows
-	// TODO: disable "Remove" button
+	Edit edit;
+	Button addbtn, removebtn;
+	ListBox lbox;
+	
+	edit.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "", IDC_EDIT, 200, 50, 100, 30);
+	addbtn.Create(*this, WS_CHILD | WS_VISIBLE, "Add", IDC_ADD, 200, 90, 100, 50);
+	removebtn.Create(*this, WS_CHILD | WS_VISIBLE | WS_DISABLED, "Remove", IDC_REMOVE, 200, 150, 100, 50);
+	lbox.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "", IDC_LB, 45, 50, 150, 150);
+
 	return 0;
 }
 
 void MainWindow::OnCommand(int id){
 	switch(id){
 		case ID_FILE_EXIT:
-			// TODO: close main window
+			OnDestroy();
 			break;
 		case ID_HELP_ABOUT:
-			// TODO: show dialog with text
+			MessageBox(*this, "NWP labos 2", "About", MB_OK);
 			break;
 		case IDC_ADD:
-			// TODO: get text from edit control
-			// TODO: add string to listbox control
-			// TODO: enable "Remove" button
+			char str[255];
+			GetDlgItemText(*this, IDC_EDIT, str, sizeof(str));
+			SendDlgItemMessage(*this, IDC_LB, LB_ADDSTRING, NULL, (LPARAM)str);
+			EnableWindow(GetDlgItem(*this, IDC_REMOVE), true);
 			break;
 		case IDC_REMOVE:
 			// TODO: get listbox selection
