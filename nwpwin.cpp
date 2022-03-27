870a203f84df26b4cdf5a62814c3a5f435d3514b
FranKalinger0998 <f.kalinger@gmail.com>
2022-03-27 16:15:34 +0200

    Reverted

--- nwpwin.cpp
+++ nwpwin.cpp
@@ -1,73 +1,82 @@
 #include "nwpwin.h"
 
-int Application::Run()
-{
-	MSG msg;
-	while (::GetMessage(&msg, NULL, 0, 0)) {
-		::TranslateMessage(&msg);
-		::DispatchMessage(&msg);
-	}
-	return msg.wParam;
-}
+namespace vsite::nwp {
 
-std::string Window::ClassName()
-{
-	return std::string();
-}
-
-bool Window::Register(const std::string& name)
-{
-	WNDCLASS wc; ZeroMemory(&wc, sizeof wc);
-	wc.lpfnWndProc = Proc;
-	wc.lpszClassName = name.c_str();
-	wc.cbWndExtra = sizeof(Window*);
-
-	wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-
-	return RegisterClass(&wc) != 0;
-}
-
-std::string Window::GenerateClassName()
-{
-	static int cnt = 1;
-	char s[16]; sprintf(s, "NWP%d", cnt++);
-	return std::string(s);
-}
-
-Window::Window() : hw(0) {}
-
-bool Window::Create(HWND parent, DWORD style, PCSTR caption, int IdOrMenu,
-	int x, int y, int width, int height)
-{
-	std::string cn = ClassName();
-	if (cn.empty())
-		Register(cn = GenerateClassName());
-	hw = ::CreateWindow(cn.c_str(), caption, style, x, y, width, height, parent, (HMENU)IdOrMenu, 0, this);
-	return hw != 0;
-}
-
-Window::operator HWND()
-{
-	return hw;
-}
-
-LRESULT CALLBACK Window::Proc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	if (message == WM_CREATE) {
-		CREATESTRUCT* pcs = reinterpret_cast<CREATESTRUCT*>(lParam);
-		Window* pw = reinterpret_cast<Window*>(pcs->lpCreateParams);
-		::SetWindowLong(hwnd, 0, reinterpret_cast<long>(pw));
-		pw->hw = hwnd;
-		return pw->OnCreate(pcs);
-	}
-
-	Window* pThis = reinterpret_cast<Window*>(::GetWindowLong(hwnd, 0));
-	switch (message)
+	int application::run()
 	{
-	case WM_COMMAND: pThis->OnCommand(LOWORD(wParam)); return 0;
-	case WM_DESTROY: pThis->OnDestroy(); return 0;
+		MSG msg;
+		while (::GetMessage(&msg, NULL, 0, 0)) {
+			::TranslateMessage(&msg);
+			::DispatchMessage(&msg);
+		}
+		return msg.wParam;
 	}
-	return ::DefWindowProc(hwnd, message, wParam, lParam);
-}
+
+	std::string window::class_name()
+	{
+		return std::string();
+	}
+
+	bool window::register_class(const std::string& name)
+	{
+		WNDCLASS wc{};
+		wc.lpfnWndProc = proc;
+		wc.lpszClassName = name.c_str();
+		wc.cbWndExtra = sizeof(window*);
+
+		wc.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+		wc.hCursor = ::LoadCursor(0, IDC_ARROW);
+		wc.hbrBackground = static_cast<HBRUSH>(::GetStockObject(WHITE_BRUSH));
+
+		return ::RegisterClass(&wc) != 0;
+	}
+
+	std::string window::generate_class_name()
+	{
+		static int n = 1;
+		return _T("NWP") + to_tstring(n++);
+	}
+
+	bool window::create(HWND parent, DWORD style, LPCTSTR caption, int id_or_menu,
+		int x, int y, int width, int height)
+	{
+		std::string cn = class_name();
+		if (cn.empty())
+			register_class(cn = generate_class_name());
+		hw = ::CreateWindow(cn.c_str(), caption, style, x, y, width, height, parent, (HMENU)id_or_menu, 0, this);
+		return hw != 0;
+	}
+
+	window::operator HWND()
+	{
+		return hw;
+	}
+
+	LRESULT CALLBACK window::proc(HWND hw, UINT msg, WPARAM wp, LPARAM lp)
+	{
+		if (msg == WM_CREATE) {
+			CREATESTRUCT* pcs = reinterpret_cast<CREATESTRUCT*>(lp);
+			window* pw = reinterpret_cast<window*>(pcs->lpCreateParams);
+			::SetWindowLong(hw, 0, reinterpret_cast<long>(pw));
+			pw->hw = hw;
+			return pw->on_create(pcs);
+		}
+
+		window* pw = reinterpret_cast<window*>(::GetWindowLong(hw, 0));
+		switch (msg)
+		{
+		case WM_COMMAND: pw->on_command(LOWORD(wp)); return 0;
+		case WM_DESTROY: pw->on_destroy(); return 0;
+		}
+		return ::DefWindowProc(hw, msg, wp, lp);
+	}
+
+	void set_icons(HINSTANCE instance, HWND window, int icon_id)
+	{
+		HICON hib = (HICON)::LoadImage(instance, MAKEINTRESOURCE(icon_id), IMAGE_ICON, 32, 32, LR_DEFAULTCOLOR);
+		::PostMessage(window, WM_SETICON, ICON_BIG, reinterpret_cast<LPARAM>(hib));
+		HICON his = (HICON)::LoadImage(instance, MAKEINTRESOURCE(icon_id), IMAGE_ICON, 16, 16, LR_DEFAULTCOLOR);
+		::PostMessage(window, WM_SETICON, ICON_SMALL, reinterpret_cast<LPARAM>(his));
+	}
+
+} // namespace
