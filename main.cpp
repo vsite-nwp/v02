3573d85e9bd692b316f706acb6ab0f7d7795b904
Filip Lončar <filoncar@vsite.hr>
2022-04-04 17:34:02 +0200

    Class button, edit, list_box overrite class_name method

--- main.cpp
+++ main.cpp
@@ -1,7 +1,47 @@
 #include "nwpwin.h"
 #include "res.h"
 
+using namespace std;
+
+//sve samo pisati u main.cpp
+
 // TODO: prepare classes (edit, button, list_box) for child windows
+// 
+
+//class edit() {};
+//class button:public vsite::nwp::window{};
+//class list_box() {};
+
+
+class button : public vsite::nwp::window {
+
+	string class_name{
+		return "button";
+	}
+};
+
+class edit : public vsite::nwp::window {
+
+	string class_name{
+		return "edit";
+	}
+}
+
+class list_box : public vsite::nwp::window {
+
+	string class_name{
+		return "list_box";
+	}
+};
+
+// izvedene klase
+// 
+// 
+// class button : public vsite::nwp::window{
+// protected:
+//	std::string class_name() override {return "BUTTON}
+// 
+// 
 // TODO: derive from window, override class_name
 
 class main_window : public vsite::nwp::window
@@ -15,6 +55,13 @@ protected:
 int main_window::on_create(CREATESTRUCT* pcs)
 {
 	// TODO: create all child windows
+	// 
+	// button b1;
+	// b1.create(*this, WS_CHILD|WS_VISIBLE, "Add", IDC_ADD
+	// 
+	// list_box lb;
+	// lb.create(*this, WS_CHILD|WS_VISIBLE|WS_BORDER
+	// 
 	// TODO: disable "Remove" button
 	return 0;
 }
@@ -33,6 +80,11 @@ void main_window::on_command(int id){
 			// TODO: enable "Remove" button
 			break;
 		case IDC_REMOVE:
+
+			//int index = SendDialogItemMEssage(*this, IDC_LB, LB_GETCURSEL,o0,0);
+			//if(index)
+			//SendDialogItemMesage
+
 			// TODO: get listbox selection
 			// TODO: if there is a selection, delete selected string
 			// TODO: disable "Remove" button if listbox is empty
