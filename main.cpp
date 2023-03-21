f533cce19cd03c18680250916b8b7476b6af2aa8
Mihael Vlainic <mivlaini@vsite.hr>
2023-03-21 08:22:08 +0100

    Vjezba2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include "nwpwin.h"
 #include "res.h"
 
-// TODO: prepare classes (edit, button, list_box) for child windows
-// TODO: derive from window, override class_name
-
 class Button : public vsite::nwp::window {
 	std::string class_name() override { return "Button"; };
 };
@@ -30,13 +27,10 @@ public:
 	Edit e;
 	Button btn1;
 	Button btn2;
-	HWND rb;
 };
 
 int main_window::on_create(CREATESTRUCT* pcs)
 {
-	// TODO: create all child windows
-	// TODO: disable "Remove" button
 
 	lb.create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, 0, IDC_LB, 50, 15, 110, 115);
 	e.create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, 0, IDC_EDIT, 190, 15, 100, 25);
@@ -51,28 +45,21 @@ int main_window::on_create(CREATESTRUCT* pcs)
 void main_window::on_command(int id) {
 	switch (id) {
 	case ID_FILE_EXIT:
-		PostQuitMessage(0);
+		if (MessageBox(*this, TEXT("Are you sure you want Exit"), TEXT("NWP"), MB_YESNO) == IDYES) {
+			PostQuitMessage(0);
+		}
 		break;
 	case ID_HELP_ABOUT:
-		// TODO: show dialog with text
-		MessageBox(NULL, TEXT("Hello"), TEXT("Hello"), MB_OKCANCEL);
+		MessageBox(*this, TEXT("Help"), TEXT("NWP"), MB_OKCANCEL);
 		break;
 	case IDC_ADD:
-		// TODO: get text from edit control
-		// TODO: add string to listbox control
-		// TODO: enable "Remove" button
 		char buffer[15];
 		GetDlgItemText(*this, IDC_EDIT, buffer, 15);
 		SendDlgItemMessage(*this, IDC_LB, LB_ADDSTRING, 0, (LPARAM)buffer);
-		rb = GetDlgItem(*this, IDC_REMOVE);
-		EnableWindow(rb, TRUE);
+		SendDlgItemMessage(*this, IDC_EDIT, WM_SETTEXT, 0, (LPARAM)"");
+		EnableWindow(btn2, TRUE);
 		break;
 	case IDC_REMOVE:
-		// TODO: get listbox selection
-		// TODO: if there is a selection, delete selected string
-		// TODO: disable "Remove" button if listbox is empty
-
-		HWND lb = GetDlgItem(*this, IDC_LB);
 		int Lbmessage = SendMessage(lb, LB_GETCURSEL, 0, 0);
 
 		if (Lbmessage != LB_ERR) {
@@ -80,8 +67,7 @@ void main_window::on_command(int id) {
 		}
 
 		if (SendMessage(lb, LB_GETCOUNT, 0, 0) == 0) {
-			rb = GetDlgItem(*this, IDC_REMOVE);
-			EnableWindow(rb, FALSE);
+			EnableWindow(btn2, FALSE);
 		}
 		break;
 	}
